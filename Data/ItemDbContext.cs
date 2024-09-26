2c22b03ffe1a43502da53e47da02dc94a2a35622
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-27 02:17:02 +0600

    installed efcore packages, added db context class, configured api connection with data base.

---
+++ Data/ItemDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EcommerceApi.Models.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Data
+{
+    public class ItemDbContext : DbContext
+    {
+        public ItemDbContext(DbContextOptions<ItemDbContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}

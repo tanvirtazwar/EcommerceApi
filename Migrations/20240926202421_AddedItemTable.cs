612dfb8f1036e5bf1fa14c0f9369500eda7f13b9
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-27 02:39:00 +0600

    created item table to the database and the type of image property has been chaged

---
+++ Migrations/20240926202421_AddedItemTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    BrandName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Ram = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Rom = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    CameraMP = table.Column<int>(type: "int", nullable: false),
+                    Images = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}

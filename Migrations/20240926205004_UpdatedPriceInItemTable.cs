5b9f050876068ab099e78869508aa24137d6fcdd
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-27 02:59:31 +0600

    added price property to item model and item database table has been modified with it.

---
+++ Migrations/20240926205004_UpdatedPriceInItemTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedPriceInItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

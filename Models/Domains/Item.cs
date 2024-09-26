78fbb7434a2f4a280887e5833a5b540c0e1694c9
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-27 01:24:43 +0600

    added item model

---
+++ Models/Domains/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.Models.Domains
+{
+    public class Item
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [StringLength(30)]
+        public string BrandName { get; set; } = string.Empty;
+        [Required]
+        [StringLength(30)]
+        public string Color { get; set; } = string.Empty;
+        [Required]
+        [StringLength(30)]
+        public string Ram { get; set; } = string.Empty;
+        [Required]
+        [StringLength(30)]
+        public string Rom { get; set; } = string.Empty;
+        [Required]
+        public int CameraMP { get; set; }
+        public string[] Images { get; set; } = new string[3];
+    }
+}
